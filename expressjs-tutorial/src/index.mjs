eb8a2d78209f084cb5f1e6efca82729a6c30c6e9
TheKiruls <amirin856@gmail.com>
2024-06-19 17:03:09 +0800

    continue with routers

--- expressjs-tutorial/src/index.mjs
+++ expressjs-tutorial/src/index.mjs
@@ -1,13 +1,30 @@
 // ECMA SCRIPT MODULE 
-import express from 'express';
+import express from 'express'; // import express
 
-const app = express(); 
+// express validator to validate incoming data for our Express API (process that data, save it to a database, submit it to another external API) using as middleware (validation that require req body) (query, body, headers, cookies, route parameters)
+import { 
+    query, 
+    validationResult, 
+    matchedData, 
+    checkSchema } from "express-validator"; 
+import { createUserValidationSchema } from './utils/validationSchemas.mjs';
+
+const app = express(); // assigning to express for use
+
+app.use(express.json()); // application sets up middleware that parses incoming request bodies containing JSON and makes the parsed data available under the `req.body` property.
 
 // register middleware
-app.use(express.json());
+const loggingMiddleware = (req,res, next) => { // middleware must be registered before routes
+    console.log(`Method: ${req.method} - url: ${req.url}`);
+    next();
+};
 
-const PORT = process.env.PORT || 3000;
+app.use(loggingMiddleware, (req,res, next) => {  //use middleware
+    console.log("Finished Logging...");
+    next();
+});
 
+// data object
 const mockUsers = [
     {id: 1, username: 'anson', displayName: 'Anson'},
     {id: 2, username: 'amirin', displayName: 'Amirin'},
@@ -17,44 +34,73 @@ const mockUsers = [
     {id: 6, username: 'marilyn', displayName: 'Marilyn'},
 ];
 
+const PORT = process.env.PORT || 3000;
 // Production Environment: In a production setting, the server (like Heroku, AWS, or any other hosting service) usually provides a port for your application to run on, which will be set in the PORT environment variable. Thus, process.env.PORT will have a value, and the application will listen on that port.
 
-app.get('/', (req, res) => {
-    res.status(200).send('Hello world');
-});
+const resolveIndexByUserId = (req, res, next) => {
+    const {params: {id}} = req; 
+    // This line extracts the id parameter from the request's URL parameters (req.params). For example, if the URL is /api/users/3, id will be 3.
+    const parsedId = parseInt(id); // to make sure its numarical value Int = integer 
+    // parseInt = Convert a string into an integer
+    if (isNaN(parsedId)) return res.sendStatus(400); // error if Id is not a number
+    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);  // search index user from mockUsers array by id matching parsedId
+    if (findUserIndex === -1) return res.sendStatus(404); // error if cant find user
+    req.findUserIndex = findUserIndex; 
+    // Stores the `findUserIndex` in the request object (`req.findUserIndex`) so it can be accessed by the next middleware or route handler.
+    next(); // eror or null argument (to pass control to the next middleware function or route handler.)
+};
 
-app.get('/api/users', (req,res) => {
-    console.log(req.query);
-    const 
-        {query: {filter, value}, // Destructuring the Query Parameters
-        // shorthand for: 
-        // const filter = req.query.filter;
-        // const value = req.query.value;
-    } = req;
-    // if(!filter && !value) return res.send(mockUsers); // when filter and value are undefined
-    if (filter && value) 
-        return res.send( // if defined then
-            mockUsers.filter((user) => user[filter].includes(value)) // check mockUsers
-        );
-    return res.send(mockUsers); // if there is only filter or value it will send mockUsers 
-});
+//  example using middleware
+app.get('/', 
+    (req, res, next) => {
+        console.log("Base URL 1");
+        next(); // call next middleware
+    },
+    (req, res, next) => {
+        console.log("Base URL 2");
+        next(); // call next middleware
+    },
+    (req, res, next) => {
+        console.log("Base URL 3");
+        next(); // call next middleware
+    },
+    (req, res) => {
+    res.status(200).send('Hello world');}
+);
 
-// POST REQUEST 
-app.post('/api/users', (req,res) => {
-    console.log(req.body);
-    const {body} = req;
-    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...body};
-    mockUsers.push(newUser);
-    return res.status(201).send(newUser);
-});
+// route to users and using query parameters
+app.get(
+    '/api/users', 
+    query('filter')
+        .isString()
+        .notEmpty().withMessage('Must not be empty')
+        .isLength({min: 3, max: 10}).withMessage('Must be at least 3-10 characters'), // validation 
+    (req,res) => {
+        // console.log(req['express-validator#contexts']);
+        const result = validationResult(req);
+        console.log(result);
+        const 
+            {query: {filter, value}, // Destructuring the Query Parameters
+            // shorthand for: 
+            // const filter = req.query.filter;
+            // const value = req.query.value;
+        } = req;
+        // if(!filter && !value) return res.send(mockUsers); // when filter and value are undefined or...
+        if (filter && value) 
+            return res.send( // if defined then
+                mockUsers.filter((user) => user[filter].includes(value)) // check mockUsers
+            );
+        return res.send(mockUsers); // else if there is only filter or value or nothing it will send mockUsers 
+    }
+); // example http://localhost:3000/api/users?filter=username&value=h (make sure to run the server)
 
-app.get('/api/users/:id', (req,res) => {
+// QUERY PARAMETERS = They are commonly used in web development to filter, sort, or provide additional details for a request. Query parameters are added to the URL after a question mark (?) and are separated by ampersands (&). Each parameter consists of a key and a value, separated by an equals sign (=).
+
+// GET REQUEST by id
+app.get('/api/users/:id', resolveIndexByUserId, (req,res) => {
     console.log(req.params); // allows you to capture dynamic values from the URL path
-    const parsedId = parseInt(req.params.id); 
-    console.log(parsedId); // access the value of the “id” parameter
-    if (isNaN(parsedId)) return res.status(404).send('Invalid ID.'); // error for invalid ID
-
-    const findUser = mockUsers.find((user) => user.id === parsedId); // find user
+    const {findUserIndex} = req;
+    const findUser = mockUsers[findUserIndex];
     if (!findUser) return res.sendStatus(404); // error for not found id such as 30 
     return res.send(findUser); // else findUser
 });
@@ -63,49 +109,45 @@ app.get('/api/products', (req,res) => {
     res.send([ {id: 123, name: 'chicken', price: 12.99}]);
 });
 
-// PUT REQUEST 
-app.put('/api/users/:id', (req,res) => {
-    const {
-        body, 
-        params: {id},
-    } = req;
-
-    const parsedId = parseInt(id); // to make sure its numarical value
-    if (isNaN(parsedId)) return res.sendStatus(400); // error for not a number
-    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId); // search user from mockUsers by id
-    if (findUserIndex === -1) return res.sendStatus(404); // error if cant find userindex
-
-    mockUsers[findUserIndex] = {id: parsedId, ...body};
-    return res.sendStatus(200);
-
+// POST REQUEST create data 
+app.post(
+    '/api/users',
+    checkSchema(createUserValidationSchema), //using schema
+    (req,res) => {
+    console.log(req.body); // example {"username": "dev"}
+    const result = validationResult(req);
+    console.log(result);
+    // errors
+    if (!result.isEmpty()) // boolean true ! false using logical operator
+        return res.status(400).send({errors: result.array()}); 
+    // push data
+    const data = matchedData(req); // match the require from validation
+    // The data property contains the data sent by the client in the POST request. After this line, data will hold the same object that was logged in the previous line.
+    const newUser = { id: mockUsers[mockUsers.length - 1].id + 1, ...data}; // -1 is for last one 
+    // This line creates a new user object. It does this by combining an id property with the properties from the data object.
+    mockUsers.push(newUser); 
+    return res.status(201).send(newUser);
 });
 
-// PATCH REQUEST 
-app.patch('/api/users/:id', (req,res) => {
-    const {
-        body, 
-        params: {id},
-    } = req;
-
-    const parsedId = parseInt(id); 
-    if (isNaN(parsedId)) return res.sendStatus(400); 
-    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId); 
-    if (findUserIndex === -1) return res.sendStatus(404);
-
-    mockUsers[findUserIndex] = {...mockUsers[findUserIndex], ...body};
+// PUT REQUEST = update entire resource for that specific id 
+app.put('/api/users/:id', resolveIndexByUserId, (req,res) => { //This route uses the `resolveIndexByUserId` middleware before executing the route handler.
+    const {body, findUserIndex} = req; 
+    //Extracts `body` and `findUserIndex` from the request object. `body` contains the data sent with the PUT request, and `findUserIndex` was set by the resolveIndexByUserId middleware.
+    mockUsers[findUserIndex] = {id: mockUsers[findUserIndex].id, ...body}; // Updates the user at `findUserIndex` in the mockUsers array.
     return res.sendStatus(200);
-}) 
+});
 
-app.delete('/api/users/:id', (req,res) => {
-    const { 
-    params: {id},
-    } = req;
+// PATCH REQUEST = update partially
+app.patch('/api/users/:id', resolveIndexByUserId, (req,res) => {
+    const {body,findUserIndex} = req;
+    mockUsers[findUserIndex] = {...mockUsers[findUserIndex], ...body}; 
+    return res.sendStatus(200);
+});
 
-    const parsedId = parseInt(id);
-    if(isNaN(parsedId)) return res.sendStatus(400);
-    const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
-    if (findUserIndex === -1) return res.sendStatus(404);
-    mockUsers.splice(findUserIndex, 1); //delete count
+// DELETE REQUEST 
+app.delete('/api/users/:id', resolveIndexByUserId, (req,res) => {
+    const {findUserIndex} = req;
+    mockUsers.splice(findUserIndex, 1); // using splice while argument findUserIndex and delete count 
     return res.sendStatus(200);
 });
 
@@ -113,12 +155,6 @@ app.listen(PORT, () => {
     console.log(`Running on ${PORT}`);
 });
 
-// localhost:3000 //domain
-// localhost:3000/products // route path
-// localhost:3000/products?key=value&key2=value2 // query string
-
-// QUERY PARAMETERS = They are commonly used in web development to filter, sort, or provide additional details for a request. Query parameters are added to the URL after a question mark (?) and are separated by ampersands (&). Each parameter consists of a key and a value, separated by an equals sign (=).
-
-// POST REQUEST = create data 
-// PUT REQUEST / PATCH = update data different ways (PATCH update partially PUT update entire resource)
-// DELETE REQUEST 
+// localhost:3000  //domain
+// localhost:3000/products  //route path
+// localhost:3000/products?key=value&key2=value2  //query string
---
+++ expressjs-tutorial/src/utils/validationSchemas.mjs
@@ -0,0 +1,10 @@
+export const createUserValidationSchema = {
+    username:{
+        isLength: { options: {min: 5,max: 30}, errorMessage: "Username must be at least 5 characters with a max of 30 characters"},
+        notEmpty: {errorMessage: "Username cannot be empty!"},
+        isString: {errorMessage: "Username must be a string!"},
+    },
+    displayName:{
+        notEmpty: true,
+    }
+};
